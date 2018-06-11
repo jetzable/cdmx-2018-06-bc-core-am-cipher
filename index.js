9d0bd0ee8076901ffdc37b5fae47816cd67ef5b4
jetzable <gjetzable@gmail.com>
2018-06-11 17:59:21 -0500

    Cifrado Cesar completo

---
+++ index.js
@@ -0,0 +1,19 @@
+function getEncode(){
+    console.log("prueba");
+    const offset = (parseInt(document.getElementById("desplazamiento").value)) % 26;
+    const string = document.getElementById("firstMessage").value;
+    document.getElementById("resultado").innerHTML = " ";
+    console.log(offset);
+    console.log(string);
+    cipher.encode(offset, string);
+};
+
+function getDecode(finalMessage){
+    console.log("prueba");
+    const offset = (parseInt(document.getElementById("desplazamiento").value)) % 26;
+    const string = document.getElementById("firstMessage").value;
+    document.getElementById("resultado").innerHTML = " ";
+    console.log(offset);
+    console.log(string);
+    cipher.decode(offset, string);
+};
